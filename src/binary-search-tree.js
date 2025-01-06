61d4c9387b21e29893be9dc88a6a77e74bb1f7f9
MaryTreasure <mari199191@mail.ru>
2025-01-06 20:35:33 +0300

    feat: implement part of BinarySearchTree task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -9,13 +9,16 @@ const { NotImplementedError } = require('../extensions/index.js');
 class BinarySearchTree {
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.root = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+
+    if(this.root === null) {
+      this.root = data;
+    } else {
+      this.remove(this.root, data)
+    }
   }
 
   has(/* data */) {
